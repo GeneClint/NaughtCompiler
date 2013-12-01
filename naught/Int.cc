bd17fb1b9efb134d11fbe37f3c6af12be42a760a
Clint Malcolm <clintm3@attu3.cs.washington.edu>
2013-11-30 22:56:05 -0800

    corrected Int

--- naught/Int.cc
+++ naught/Int.cc
@@ -1,11 +1,23 @@
+#include <string>
+#include <Term.cc>
+
 class Int: public Term {
 public:
-  Int (int);
-
+  Int (int) : Term ();
+  Int (string) : Term ();
+  int getValue() const;
 private:
   int val;
+};
+
+Int::Int(string v) {
+  val = std::stoi( v );
 }
 
-  Int::Int(int v) {
-    val = v;
-  }
+Int::Int (int v) {
+  val = v;
+}
+
+Int::int getValue() const {
+  return val;
+}
