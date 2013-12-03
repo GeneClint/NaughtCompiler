2bb0c4b4247d637894130c277caf7c3830cbc920
Clint Malcolm <clintm3@attu1.cs.washington.edu>
2013-12-03 00:44:10 -0800

    added funcdef, block, and statement writing, assumes expression and vardecl works correctly

--- naught/NaughtParser.cc
+++ naught/NaughtParser.cc
@@ -43,3 +43,46 @@ void NaughtParser::writeFunctionDecl(FuncDecl f) {
   }
   out << " );" << endl;
 }
+
+void NaughtParser::writeFunctionDef(FuncDef f) {
+  out << "int " << f.getId().toString() << " ( ";
+  out << "int " << f.getId().toString() << " ( ";
+  auto params = f.getParams();
+  if (params.size() > 0) {
+    out << params[0].toString();
+    for(size_t i = 1; i < params.size(); i++) {
+      out << " , " << params[i].toString();
+    }
+  }
+  out << " ) " << endl;
+  if (bloc != NULL) {
+    writeBlock(*bloc);
+  }
+}
+
+void NaughtParser::writeBlock(Block b) {
+  out << " { " << endl;
+  auto decls = b..getVarDecls();
+  for(auto decl : decls) {
+    writeVarDecl(decl);
+    out << endl;
+  }
+  auto stmts = b.getStatements();
+  for(auto stmt : stmts) {
+    writeStatement(stmt);
+    out << endl;
+  }
+  out << " } " << endl;
+}
+
+void NaughtParser::writeStatement(Statement s) {
+  auto exp = s.getExpression();
+  string tempvar;
+  if (exp != NULL) {
+    tempvar = writeExpression(*exp);
+  }
+  if (s.isReturn()) {
+    out << "return " << tempvar;
+  }
+  out << endl;
+}
