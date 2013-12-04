3210e35675937ec319b48b1590ffd4ce456cc608
Gene Kim <rfpnq38@yahoo.com>
2013-12-03 22:38:11 -0800

    added an int test program

---
+++ programs/ints1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (  ) 
+ { 
+int temp0 = ;
+x = int temp0;
+int temp1 = ;
+y = int temp1;
+int temp2 = ;
+
+
+int temp3 = ;
+
+
+int temp4 = ;
+return int temp4;
+
+ } 
+
