26f57845b66be411d70ac3d97569d0d07191c6af
Gene Kim <rfpnq38@yahoo.com>
2013-12-06 16:47:28 -0800

    added headers with our names

--- naught/Int.cc
+++ naught/Int.cc
@@ -1,3 +1,6 @@
+// Clint Malcolm
+// 1134490
+// clintm@cs.washington.edu
 // Gene Kim
 // 1130267
 // genelkim@uw.edu
--- naught/comp.cc
+++ naught/comp.cc
@@ -1,4 +1,7 @@
 /* main.cc */
+// Clint Malcolm
+// 1134490
+// clintm@cs.washington.edu
 // Gene Kim
 // 1130267
 // genelkim@uw.edu
